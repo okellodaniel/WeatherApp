12d1823ddd62a8a18056c97335b8f676e0f0aa51
okello daniel <handsdani@gmail.com>
2022-05-03 21:10:41 +0300

    Adding connection to OpenWeather Api

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+import requests  
 
 # Create your views here.
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=f245e17d2076088c97ddb34f14a75c86'
+    city = 'Las Vegas'
+
+    city_weather = requests.get(url.format(city)).json()
+
+    template = loader.get_template('weather/index.html')
+
+    return HttpResponse(template.render(request))
