8b5bb385fd122621109304fc2c63f9d4f5a82188
okello daniel <handsdani@gmail.com>
2022-05-03 21:46:41 +0300

    Adding form to add city

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=25)
+
+    # show actual name of the city on the dashboard
+    
+    def __str__(self):
+        return self.name
+    
+    # Show the name of the city in plural form
+    class Meta:
+        verbose_name_plural = 'Cities'
--- weather/views.py
+++ weather/views.py
@@ -2,15 +2,35 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 import requests  
+from .models import City
 
 # Create your views here.
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=f245e17d2076088c97ddb34f14a75c86'
-    city = 'Las Vegas'
+    
+    weatherData = []
+    # Return all the cities in the database
+
+    city = City.objects.all()
 
     city_weather = requests.get(url.format(city)).json()
+    print(city_weather)
 
     template = loader.get_template('weather/index.html')
 
-    return HttpResponse(template.render(request))
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon'],
+    }
+
+    # Add weather to the list
+    weatherData.append(weather)
+
+    context = {
+        'weatherData': weatherData
+    }
+
+    return HttpResponse(template.render(context, request))
