fe24801cb169a00db1364f972ffb679b49a633e7
okello daniel <handsdani@gmail.com>
2022-05-03 23:27:04 +0300

    final app

--- weather/views.py
+++ weather/views.py
@@ -3,34 +3,43 @@ from django.template import loader
 from django.http import HttpResponse
 import requests  
 from .models import City
-
+from .forms import CityForm
 # Create your views here.
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=f245e17d2076088c97ddb34f14a75c86'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=f245e17d2076088c97ddb34f14a75c86'
     
-    weatherData = []
+
     # Return all the cities in the database
+    cities = City.objects.all()
 
-    city = City.objects.all()
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
-    city_weather = requests.get(url.format(city)).json()
-    print(city_weather)
+    form = CityForm()
+
+    weatherData = []
 
     template = loader.get_template('weather/index.html')
 
-    weather = {
-        'city' : city,
-        'temperature' : city_weather['main']['temp'],
-        'description' : city_weather['weather'][0]['description'],
-        'icon' : city_weather['weather'][0]['icon'],
-    }
+    for city in cities:
 
-    # Add weather to the list
-    weatherData.append(weather)
+        city_weather = requests.get(url.format(city)).json()
+
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon'],
+        }
+
+        # Add weather to the list
+        weatherData.append(weather)
 
     context = {
-        'weatherData': weatherData
+        'weatherData': weatherData,
+        'form' : form
     }
 
     return HttpResponse(template.render(context, request))
